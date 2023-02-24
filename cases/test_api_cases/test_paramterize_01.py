4797574871a8250960b96f607d5ea6024e1d6a98
liuyinggang <952904268@qq.com>
2023-02-24 18:41:36 +0800

    last

--- cases/test_api_cases/test_paramterize_01.py
+++ cases/test_api_cases/test_paramterize_01.py
@@ -8,7 +8,10 @@ import requests
 import json
 from untils.get_data import get_data
 
+url = "https://api.apiopen.top/api/login/"
 param = [
+    ({"Content-Type": "application/json"}, {"account": "123456@qq.com", "password": "123456"}),
+    ({"Content-Type": "application/json"}, {"account": "123456@qq.com", "password": "123456"}),
     ({"Content-Type": "application/json"}, {"account": "123456@qq.com", "password": "123456"})
 ]
 
@@ -23,15 +26,18 @@ class Test_model(object):
 
     @allure.description('一个登录验证的测试')
     @pytest.mark.parametrize("headers, paydata", param)
-    def test_parametrize_login_error(self, headers, paydata):
-
-        url = 'https://api.apiopen.top/api/login/'
-
+    def test_parametrize_login_status_success(self, headers, paydata):
         r = requests.post(url=url,
                           data=paydata,
                           headers=headers)
+        assert r.status_code != 200
+        # print(r.json())
+        # result = r.json()
+        # assert result['code'] == 403
 
-        # assert r.status_code == 200
-        print(r.json())
+    @allure.description('验证登录错误的测试')
+    @pytest.mark.parametrize("headers, paydata", param)
+    def test_paramterize_login_code_error(self, headers, paydata):
+        r = requests.post(url=url,data=paydata, headers=headers)
         result = r.json()
         assert result['code'] == 400
